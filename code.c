e8961a0276b408f84bd746e17e72d0d2d2f15bfa
wilsionlo2468 <67331711+wilsionlo2468@users.noreply.github.com>
2023-04-19 12:56:33 +0800

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,10 @@
 // Your C code here
+#include <stdio.h>
+
+int main() {
+    //this is simple for loop
+    double size = 3.251;
+    for(int i = 0 ; i < size ; i++ )
+        printf("Hello csci3215_%d\n" , i);
+    return 0;
+}
